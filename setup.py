a2b53f5026afb8c4c25f27ddcfd49c0ebc6e2818
Peter Dowling <peter.dowling9@mail.dcu.ie>
2019-04-08 19:54:51 +0100

    Updated table divided versionID into publication_id and publication_version for ease of tracking/analysis of multiple versions of publications. Cleaned up title subject regex.

--- setup.py
+++ setup.py
@@ -16,11 +16,12 @@ engine = create_engine('sqlite:///test.db')
 metadata = MetaData()
 
 arxiv_cs_publications = Table('arxiv_cs_publications', metadata,
-    Column('publication_id', Integer, primary_key=True),
+    Column('entry_id', Integer, primary_key=True),
     Column('title', String),
     Column('description', String),
     Column('subject', String),
-    Column('version', String),
+    Column('publication_id', String),
+    Column('publication_version', String),
     Column("created_date",DateTime, default=datetime.datetime.utcnow)
     ,)
 
@@ -41,28 +42,34 @@ for i in soup.find_all('item'):
     TitleMetaRegex_version = re.compile(r'(\d.\d*.\d.\d*\D\d.)')
     VersionID =  TitleMetaRegex_version.search(TitleMeta).group()
 
+    #split version id string into publication_id and publication_version
+    SplitID = re.split('[v]',VersionID)
+    ID = SplitID[0]
+    Version = SplitID[1]
+
     #Check if the ID exits in the database
-    s = select([arxiv_cs_publications.c.publication_id]).where(arxiv_cs_publications.c.version == VersionID)
+    s = select([arxiv_cs_publications.c.publication_id]).where(arxiv_cs_publications.c.publication_id == ID).where(arxiv_cs_publications.c.publication_version==Version)
     results = conn.execute(s)
     sqlReturn = results.fetchall()
 
     #If the ID does not exists in the database
     if not sqlReturn:
-        #capture subject from the title eg:  [cs.DS]
+
+        #Title less the meta data
         TitleText= re.sub(TitleMetaRegex,'', RawTitleText)
-        TitleMetaRegex_subject = re.compile(r'\[(.*?)\]')
-        TitleSubject = TitleMetaRegex_subject.search(TitleMeta).group()
-        #remove leading '[' and trailing ']'
-        TitleSubject = TitleSubject[1:-1]
+
+        #capture subject from the title eg:  [cs.DS]
+        TitleSubject = re.findall(r'\[(.*?)\]',RawTitleText)
+        TitleSubject = TitleSubject[0]
 
         #Get the description
         Description = i.description.get_text()
         #Remove the leading and trailing <p>
         Description = Description.replace("<p>", "").replace("</p>", "")
 
-        ins = ins_publication.values(title=TitleText,description=Description,subject=TitleSubject,version=VersionID)
-        logging.info('inseting record for %s', VersionID)
+        insert = ins_publication.values(title=TitleText,description=Description,subject=TitleSubject,publication_id=ID, publication_version=Version)
+        logging.info('inseting record for %s version %s', ID, Version)
 
-        result = conn.execute(ins)
+        conn.execute(insert)
     else:
         logging.info('record for %s exists in the database, nothing to add',VersionID)
